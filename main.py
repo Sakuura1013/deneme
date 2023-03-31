314464e2819aa866362d6d49554c7f00e1dc0d9f
Sakura <105538435+Sakuura1013@users.noreply.github.com>
2023-03-31 21:25:30 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+meme_sozlugu = {
+    "LOL": "Komik bir şeye verilen cevap",
+    "CRINGE": "Garip ya da utandırıcı bir şey",
+    "ROFL": "Bir şakaya karşılık cevap",
+    "SHEESH": "Onaylamamak",
+    "CREEPY": "Korkunç",
+    "TO AGGRO": "Agresifleşmek/Sinirlenmek"
+}
+
+print("Hoşgeldiniz!")
+
+while True:
+    kelime = input("Lütfen anlamadığınız bir kelimeyi büyük harf kullanarak yazın:")
+    
+    if kelime in meme_sozlugu.keys():
+        print(meme_sozlugu[kelime])
+    else:
+        print("Bu kelime sözlüğümüzde yok.")
